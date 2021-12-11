8feb9bcc4afca1da720c1ad54acc57cec0292c22
Philky101 <philky101@gmail.com>
2021-12-10 21:05:32 -0500

    Sprite Class

---
+++ Sprite.py
@@ -0,0 +1,15 @@
+class Sprite:
+
+    def __init__ (self, name, hp, dmg, image):
+        self.name = name
+        self.hp = hp
+        self.dmg = dmg
+        self.image = image
+        self.items = []
+
+    def attack (self):
+        true == true
+        #do attack animation
+
+    def attacked (self, hp):
+        self.hp = self.hp - dmg
